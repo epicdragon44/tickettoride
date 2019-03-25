59f7403576a2d54c4c3635181386d383e86c27e7
jerryworkouts <47676116+jerryworkouts@users.noreply.github.com>
2019-03-25 14:23:30 -0500

    asdf

---
+++ src/ContractDeck.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.io.*;
+
+public class ContractDeck {
+	private Queue<Contract> deck;
+
+	public ContractDeck() throws IOException {
+		Scanner s = new Scanner(new File("elleh"));
+		deck = LinkedList<Contract>();
+	}
+	private ArrayList<Contract> draw(int num)
+	{
+		ArrayList<Contract> c = new ArrayList<Contract>();
+		for(int i = 0; i <num;i++)
+		{
+			deck.remove();
+		}
+	}
+}
