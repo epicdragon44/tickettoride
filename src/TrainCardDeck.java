c0986a798a9d9cae5032fd58bda63e63bc114300
jerryworkouts <47676116+jerryworkouts@users.noreply.github.com>
2019-03-26 14:03:57 -0500

    Update TrainCardDeck.java

--- src/TrainCardDeck.java
+++ src/TrainCardDeck.java
@@ -1,17 +1,46 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TrainCardDeck {
-    private ArrayList<TrainCard> deck;
+	private ArrayList<TrainCard> deck;
 
-    public TrainCardDeck() {
-        deck = new ArrayList<TrainCard>();
-    }
+	public TrainCardDeck() {
+		deck = new ArrayList<TrainCard>();
+		restartDeck();
+	}
 
-    public void restartDeck() {
+	public void restartDeck() {
+		for (int i = 0; i < 12; i++) {
+			deck.add(new TrainCard("", true));
+		}
+		for (int i = 0; i < 12; i++) {
+			deck.add(new TrainCard("Orange", false));
+		}
+		for (int i = 0; i < 12; i++) {
+			deck.add(new TrainCard("White", false));
+		}
+		for (int i = 0; i < 12; i++) {
+			deck.add(new TrainCard("Red", false));
+		}
+		for (int i = 0; i < 12; i++) {
+			deck.add(new TrainCard("Green", false));
+		}
+		for (int i = 0; i < 12; i++) {
+			deck.add(new TrainCard("Black", false));
+		}
+		for (int i = 0; i < 12; i++) {
+			deck.add(new TrainCard("Blue", false));
+		}
+		for (int i = 0; i < 12; i++) {
+			deck.add(new TrainCard("Yellow", false));
+		}
+		for (int i = 0; i < 12; i++) {
+			deck.add(new TrainCard("Purple", false));
+		}
+		Collections.shuffle(deck);
+	}
 
-    }
-
-    public TrainCard draw(int index) {
-        return deck.remove(index);
-    }
+	public TrainCard draw(int index) {
+		return deck.remove(index);
+	}
 }
