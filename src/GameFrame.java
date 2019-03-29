1928dead182035aea5dac3c98de041ef84dca678
jerryworkouts <47676116+jerryworkouts@users.noreply.github.com>
2019-03-29 13:52:59 -0500

    Update GameFrame.java

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -2,21 +2,21 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameFrame extends JFrame {
-    private GamePanel gamePanel;
-    public GameFrame() {
-        super();
-        setSize(new Dimension(1000, 1000));
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
+	private GamePanel gamePanel;
 
-        gamePanel = new GamePanel();
-        add(gamePanel);
+	public GameFrame(String str) {
+		super(str);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new FlowLayout());
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		gamePanel = new GamePanel();
+		add(gamePanel);
 
-        setVisible(true);
-    }
-    public static void main(String[] args) 
-    {
-    	GameFrame game = new GameFrame();
-    }
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		GameFrame game = new GameFrame("Ticket to Ride");
+	}
 }
