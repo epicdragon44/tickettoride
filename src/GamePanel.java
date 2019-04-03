2c426ac0b3353a94c02b768e46f04c721245aa46
jerryworkouts <47676116+jerryworkouts@users.noreply.github.com>
2019-04-03 14:22:15 -0500

    fuck u krish

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,11 +2,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.IOException;
 
 public class GamePanel extends JPanel implements MouseListener {
-	private GameEngine g;
+	private GameEngine game;
+	private Color red, blue, yellow, green, dgreen, gray, gold;
 
-	public GamePanel() {
+	public GamePanel() throws IOException {
+		blue = new Color(98, 151, 255);
+		red = new Color(255, 88, 88);
+		yellow = Color.yellow;
+		green = new Color(105, 242, 105);
+		dgreen = new Color(67, 216, 67);
+		gray = new Color(205, 208, 205);
+		gold = new Color(218, 218, 4);
+		 game = new GameEngine();
 		setLayout(null);
 		setPreferredSize(new Dimension(1000, 1000));
 		setVisible(true);
@@ -19,6 +29,8 @@ public class GamePanel extends JPanel implements MouseListener {
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
+		g.setColor(gold);
+		g.fillRect(100, 100, 100, 100);
 
 	}
 
