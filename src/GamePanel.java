547d655f096cebf3ca58049abd6b7c8018bb6bd5
jerryworkouts <47676116+jerryworkouts@users.noreply.github.com>
2019-04-04 13:50:06 -0500

    Update GamePanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -9,83 +9,96 @@ import java.io.File;
 import java.io.IOException;
 
 public class GamePanel extends JPanel implements MouseListener {
-    private GameEngine game;
-    private Color red, blue, yellow, green, dgreen, gray, gold;
-    
-    public GamePanel() {
-        blue = new Color(98, 151, 255);
-    		red = new Color(255, 88, 88);
-    		yellow = Color.yellow;
-    		green = new Color(105, 242, 105);
-    		dgreen = new Color(67, 216, 67);
-    		gray = new Color(205, 208, 205);
-    		gold = new Color(218, 218, 4);
-    		 game = new GameEngine();
-    		setLayout(null);
-    		setPreferredSize(new Dimension(1000, 1000));
-    		setVisible(true);
-    }
-    
-    public void initGame() {
-    	
-    }
-    
-    public void lastRound() {
-    	
-    }
+	private GameEngine game;
+	private Color red, blue, yellow, green, dgreen, gray, gold;
+	private Font f;
 
-    public void play() {
+	public GamePanel() throws IOException {
+		blue = new Color(98, 151, 255);
+		red = new Color(255, 88, 88);
+		yellow = Color.yellow;
+		green = new Color(105, 242, 105);
+		dgreen = new Color(67, 216, 67);
+		gray = new Color(205, 208, 205);
+		gold = new Color(218, 218, 4);
+		// game = new GameEngine();
+		f = new Font("Brush Script MT", Font.BOLD, 20);
+		setLayout(null);
+		setPreferredSize(new Dimension(1000, 1000));
+		setVisible(true);
+	}
 
-    }
+	public void initGame() {
 
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        drawBackground(g);
-    }
-    public void drawConnection(Node n1, Node n2) {
+	}
 
-    }
-    public void drawBoard(Graphics g) {
+	public void lastRound() {
 
-    }
-    public void drawTracks(Graphics g) {
+	}
 
-    }
-    public void drawDecks(Graphics g) {
+	public void play() {
 
-    }
-    public void drawCities(Graphics g) {
+	}
 
-    }
-    public void drawNumbers(Graphics g) {
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		drawBackground(g);
+	}
 
-    }
-    public void drawBackground(Graphics g) {
-        try {
-            BufferedImage backgroundImg = ImageIO.read(new File("Background.png"));
-            g.drawImage(backgroundImg, 0, 0, new ImageObserver() {
-                @Override
-                public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
-                    return false;
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public void drawHand(Graphics g) {
+	public void drawConnection(Node n1, Node n2) {
 
-    }
-    public void drawRankings(Graphics g) {
+	}
 
-    }
-    public void drawCards(Graphics g) {
+	public void drawBoard(Graphics g) {
 
-    }
-    public void drawContracts(Graphics g) {
+	}
 
-    }
+	public void drawTracks(Graphics g) {
+
+	}
+
+	public void drawDecks(Graphics g) {
+
+	}
+
+	public void drawCities(Graphics g) {
+
+	}
+
+	public void drawNumbers(Graphics g) {
+
+	}
+
+	public void drawBackground(Graphics g) {
+		try {
+			BufferedImage backgroundImg = ImageIO.read(new File("Background.png"));
+			g.drawImage(backgroundImg, 0, 0, new ImageObserver() {
+				@Override
+				public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
+					return false;
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void drawHand(Graphics g) {
+
+	}
+
+	public void drawRankings(Graphics g) {
+
+	}
+
+	public void drawCards(Graphics g) {
+
+	}
+
+	public void drawContracts(Graphics g) {
+
+	}
 
 	public void play() {
 
