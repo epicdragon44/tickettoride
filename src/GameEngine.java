b43f0c95856f22089c4771c51687549942f10eb6
KAChhabra <47868779+KAChhabra@users.noreply.github.com>
2019-04-30 14:38:03 -0500

    gay

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -149,7 +149,7 @@ public class GameEngine {
 			{
 				players[currentPlayer].drawTrainCards(rtn);
 				if(!haveTrainCards())
-					tableDeck[pos]=tDeck.draw();
+					tableDeck[pos]=null;
 				else
 					tableDeck[pos]=tDeck.draw();
 			}
